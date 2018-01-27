40c484ac7fd15eb935355c4da3b90b24e17c2eb2
Matthias Lücke <ml@9bt.de>
2018-01-27 13:49:22 +0100

    initial commit with newly created nuget project

---
+++ ggj2018/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ggj2018
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var game = new LabEscapeGame()) {
+                game.Run();
+            }
+                
+        }
+    }
+}
