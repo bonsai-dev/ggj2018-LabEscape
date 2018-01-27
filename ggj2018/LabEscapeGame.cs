4ae1d12b89a0133853e1c1fae483021fe840cde6
kasoban <git@kasoban.de>
2018-01-27 16:31:27 +0100

    First basic sprite render

--- ggj2018/LabEscapeGame.cs
+++ ggj2018/LabEscapeGame.cs
@@ -7,14 +7,16 @@ namespace ggj2018
 {
     public class LabEscapeGame: Game
     {
-
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D texture;
+        Vector2 position;
 
         public LabEscapeGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            position = new Vector2(0, 0);
         }
 
         /// <summary>
@@ -26,7 +28,14 @@ namespace ggj2018
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            //texture = new Texture2D(this.GraphicsDevice, 100, 100);
+            //Color[] colorData = new Color[100 * 100];
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    float ratio = i / 10000f;
+            //    colorData[i] = new Color(1-ratio, ratio, ratio);
+            //}
+            //texture.SetData<Color>(colorData);
             base.Initialize();
         }
 
@@ -38,7 +47,7 @@ namespace ggj2018
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            texture = this.Content.Load<Texture2D>("Player_Scientist_01_Down");
             // TODO: use this.Content to load your game content here
         }
 
@@ -49,6 +58,7 @@ namespace ggj2018
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
+            Content.Unload();
         }
 
         /// <summary>
@@ -62,7 +72,11 @@ namespace ggj2018
                 Exit();
 
             // TODO: Add your update logic here
-
+            position.X += 1 * gameTime.ElapsedGameTime.Milliseconds / 5;
+            if(position.X > this.GraphicsDevice.Viewport.Width)
+            {
+                position.X = 0 - texture.Width;
+            }
             base.Update(gameTime);
         }
 
@@ -75,7 +89,9 @@ namespace ggj2018
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
