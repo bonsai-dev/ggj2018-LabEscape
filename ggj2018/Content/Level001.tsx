36f3031578ec7c2ec65dff06a85bc6fbe07c57f6
Florian Köther <Florian.Koether@web.de>
2018-01-27 19:45:47 +0100

    Added Background for Level001

---
+++ ggj2018/Content/Level001.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Level001" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="lvl_lab_unpassable_01.png"/>
+  <objectgroup draworder="index">
+   <object id="6" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="lvl_lab_passable_01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="lvl_lab_finish_01.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="lvl_lab_start_01.png"/>
+ </tile>
+</tileset>
